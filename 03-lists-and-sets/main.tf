2118121f4d7593e14b35dcf2b4fd82803a5f9689
28-png <murphy.d.matthew28@gmail.com>
2021-06-23 14:07:45 -0500

    updated

---
+++ 03-lists-and-sets/main.tf
@@ -0,0 +1,27 @@
+variable "names" {
+  default = ["ravs", "sats", "matt", "pat", "tom"]
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.46.0"
+    }
+  }
+}
+ 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  # VERSION IS NOT NEEDED HERE
+}
+
+
+resource "aws_iam_user" "my_iam_user" {
+    #count = length(var.names)
+    #name = var.names[count.index]
+    for_each = toset(var.names)
+    name = each.value
+
+}
