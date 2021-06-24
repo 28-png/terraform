a5305c28047ffb2ae850a5235d66d692556064b8
28-png <murphy.d.matthew28@gmail.com>
2021-06-24 14:31:40 -0500

    updated

--- 05-ec2-instances/outputs.tf
+++ 05-ec2-instances/outputs.tf
@@ -1,12 +1,7 @@
-output "my_s3_versioning" {
-    value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
+output "aws_security_group_http_server_details" {
+  value = aws_security_group.http_server_sg
 }
 
-output "my_s3_bucket_complete_details" {
-    value = aws_s3_bucket.my_s3_bucket
-}
 
-output "my_iam_user_complete_details" {
-    value = aws_iam_user.my_iam_user
-}
+
 
