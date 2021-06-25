0d88c552e895ac69f595bd8843086b55a2c3727e
28-png <murphy.d.matthew28@gmail.com>
2021-06-25 10:16:36 -0500

    updated

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -6,10 +6,40 @@ terraform {
     }
   }
 }
-
+ 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
   # VERSION IS NOT NEEDED HERE
 }
 
+resource "aws_security_group" "http_server_sg" {
+  name = "http_server_sg"
+  vpc_id = "vpc-0f2cb572"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+     from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "http_server_sg"
+  }
+
+}
