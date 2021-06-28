ca632a3872c26ab17c86e401f214ab99200f6f7a
28-png <murphy.d.matthew28@gmail.com>
2021-06-28 08:51:52 -0500

    updated

--- 05-ec2-instances/main.tf
+++ 05-ec2-instances/main.tf
@@ -1,3 +1,7 @@
+variable "aws_key_pair" {
+  default = "~/aws/aws_keys/default-ec2.pem"
+}
+
 terraform {
   required_providers {
     aws = {
@@ -6,40 +10,68 @@ terraform {
     }
   }
 }
- 
+
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
   # VERSION IS NOT NEEDED HERE
 }
 
+resource "aws_default_vpc" "default" {
+  
+}
+
 resource "aws_security_group" "http_server_sg" {
-  name = "http_server_sg"
+  name   = "http_server_sg"
   vpc_id = "vpc-0f2cb572"
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-     from_port = 0
-    to_port = 0
-    protocol = -1
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     name = "http_server_sg"
+  }
+
+}
+
+resource "aws_instance" "http_server" {
+  ami                    = "ami-0ab4d1e9cf9a1215a"
+  key_name               = "default-ec2"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+  subnet_id              = "subnet-b85d5bb6"
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd -y",
+      "sudo service httpd start",
+      "echo Welcome to Matt - Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html"
+    ]
   }
 
 }
